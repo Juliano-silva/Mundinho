2d04edbf619a21df2953dc482884b8f7ff68457e
Juliano-silva <88514570+Juliano-silva@users.noreply.github.com>
2023-02-11 00:18:18 -0300

    FIM

--- Script.js
+++ Script.js
@@ -1,8 +1,6 @@
 // Pedra Papel e Tesoura
 function Escolhas(){
 const Computador = Math.floor(Math.random() * 3)
-const Placar = document.getElementById("JOGO")
-const Placar1 = document.getElementById("JOGO1")
 const Jogador = document.querySelector("select").selectedIndex
 const Result = document.getElementById("Resultado");
 if (Jogador == "0"){
@@ -30,43 +28,44 @@ if (Jogador == "0" && Computador == "0"){
 }
 else if (Jogador == "0" && Computador == "1"){
     Result.innerText = "Você Perdeu"
-    ComputadorPoint +=1
+    ComputadorPoint ++
 }
 else if (Jogador == "0" && Computador == "2"){
     Result.innerText = "Você Ganhou"
-    JogadorPoint += 1
+    JogadorPoint ++
 }
 // Papel Ganhou
 else if (Jogador == "1" && Computador == "0"){
     Result.innerText = "Você Ganhou"
-    JogadorPoint += 1
+    JogadorPoint ++
 }
 else if (Jogador == "1" && Computador == "1"){
     Result.innerText = "Empate"
 }
 else if (Jogador == "1" && Computador == "2"){
     Result.innerText = "Você Perdeu"
-    ComputadorPoint +=1
+    ComputaComputadorPoint ++
 }
 // Pedra
 else if (Jogador == "2" && Computador == "0"){
     Result.innerText = "Você Perdeu"
-    ComputadorPoint += 1
+    ComputadorPoint ++
 }
 else if (Jogador == "2" && Computador == "1"){
     Result.innerText = "Você Ganhou"
-    JogadorPoint += 1
+    JogadorPoint ++
 }
 else if (Jogador == "2" && Computador == "2"){
     Result.innerText = "Empate"
 }
-console.log(JogadorPoint)
+
 }
 // SideBar
 var buttonSidebar = document.getElementById("MenuICone1")
 var buttonS = document.getElementById("MenuICone")
 var Sidebar = document.getElementById("Sidebar")
 var Rota = document.getElementById("ROTA")
+var Corpo = document.querySelector("body")
 buttonSidebar.addEventListener("click",Abrir)
 buttonS.addEventListener('click',Sair)
 function Abrir(){
@@ -75,13 +74,15 @@ function Abrir(){
     buttonSidebar.style.display="none"
     Rota.style.width = "90%"
     Rota.style.marginLeft = "10%"
+    Corpo.style.overflow = "hidden"
 }
 function Sair(){
     Sidebar.style.display="none"
     buttonS.style.display="none"
     buttonSidebar.style.display="block"
-    Rota.style.width = "100%"
-    Rota.style.marginLeft = "0%"
+    Rota.style.width = "100vw"
+    Rota.style.marginLeft = "0"
+    Corpo.style.overflow = "auto"
 }
 // Random
 // Random com Cor
@@ -107,9 +108,23 @@ document.getElementById("Cor").style.background = (Random(6))
 }
 function Image(){
     const GerarImg = (RandomImage(7))
-    document.getElementById("Image").src = GerarImg + ".jpg"
+    document.getElementById("ImageRandom").src = GerarImg + ".jpg"
     console.log(GerarImg + ".jpg")
 }
 function Copiar(){
     navigator.clipboard.writeText(CorRandom(6))
 }
+// Texto Configurações
+function Palavras(){
+    var Palavra = document.getElementById("Input").value
+    document.getElementById("Texto1").innerText = "Palavra Normal: " +  Palavra.toString()
+    document.getElementById("Texto2").innerText = "Palavra Minúscula: " + Palavra.toLocaleLowerCase()
+    document.getElementById("Texto3").innerText = "Número de Palavras com espaços: " + Palavra.length
+    document.getElementById("Texto4").innerText = "Número de Palavras sem espaços: " + Palavra.replace(/\s/g,"").length
+    document.getElementById("Texto5").innerText = "Palavra Côdigo Point: " + Palavra.codePointAt()
+    document.getElementById("Texto6").innerText = "Palavra Tirando os Espaços: " + Palavra.replace(/\s/g,"")
+    document.getElementById("Texto7").innerText = "Palavra Tirando os Espaços e colocado - : " + Palavra.replace(/\s/g,"-")
+    document.getElementById("Texto8").innerText = "Palavra Separadas: " + Palavra.split("")
+    document.getElementById("Texto9").innerText = "Primeira Palavra: " + Palavra.charAt(0)
+    document.getElementById("Texto10").innerText = "Palavra Maiúsculas : " + Palavra.toUpperCase()
+}// Pokédex
